19fd3e86780e91a234f157dfb6cb25aa756cbf52
henriquemarrocos <47908925+henriquemarrocos@users.noreply.github.com>
2020-12-23 00:15:07 -0500

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>
+          Add an Item...
+          <br/>
+          <input 
+            type="text"
+            placeholder="Type item here..."
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <button onClick={() => this.addItem()}>
+            Add
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
+//function App() {
+//  return (
+//    <div className="App">
+//      
+//    </div>
+//  );
+//}
+//
 export default App;
