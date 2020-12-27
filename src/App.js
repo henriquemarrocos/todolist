961ab8e331b881d287e8ae23cbb5be75f70664b9
henriquemarrocos <henriquemarrocos@hotmail.com>
2020-12-27 02:19:55 -0500

    Adding functions to add and remove an item

--- src/App.js
+++ src/App.js
@@ -9,6 +9,45 @@ class App extends Component {
       list:[]
     }
   }
+
+  updateInput(key, value){
+    //update react state
+    this.setState({
+      [key]: value
+    });
+  }
+
+  addItem(){
+    //create item with unique id
+    const newItem={
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //copy of current list of items
+    const list = [...this.state.list];
+
+    //add new item to list
+    list.push(newItem);
+
+    //update state with new list and reset newItem input
+    this.setState({
+      list,
+      newItem:""
+    });
+  }
+
+  deleteItem(id){
+    //copy current list of items
+    const list = [...this.state.list];
+
+    //filter out item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+
+    //update state with new list without the deleted item
+    this.setState({list: updatedList});
+  }
+  
   render() {
     return (
       <div className="App">
@@ -24,6 +63,19 @@ class App extends Component {
           <button onClick={() => this.addItem()}>
             Add
           </button>
+          <br/>
+          <ul>
+            {this.state.list.map(item => {
+              return(
+                <li key={item.id}>
+                  {item.value}
+                  <button
+                    onClick={() => this.deleteItem(item.id)}
+                  ></button>
+                </li>
+              )
+            })}
+          </ul>
         </div>
       </div>
     );
